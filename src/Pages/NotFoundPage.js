c324096794be1fa2983558c1b8c2ebf87de0f08b
1w11111837JuanFacundoConforte <111837@tecnicatura.frc.utn.edu.ar>
2022-03-05 17:12:01 -0300

    ProyectoFinal

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className="text-center">
       <h1>Error 404</h1>
       <p>No se encontro la pagina...</p>
-      <Link to="/">Volver a la Home</Link>
+      <Link to="/" className="nav-link">Volver a la Home</Link>
     </div>
   );
 };
